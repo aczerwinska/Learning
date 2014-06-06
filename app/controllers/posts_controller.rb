cb5c1d2ef826d8872fb22ce10b32fca2932d15f7
aczerwinska <czerwinska.agnieszka.lucja@gmail.com>
2014-06-06 11:00:34 +0200

    5_11

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,21 @@ def index
 	@posts = Post.all
 end
 
+def edit
+	@post = Post.find(params[:id])
+end
+
+def update
+	@post = Post.find(params[:id])
+
+	if @post.update(post_params)
+		redirect_to @post
+	else
+		render 'edit'
+	end
+	
+end
+
 private
 	def post_params
 		params.require(:post).permit(:title, :text)
