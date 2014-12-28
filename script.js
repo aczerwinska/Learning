22da77d39990ce64c5d58b2dcf80984c909b7eae
aczerwinska <czerwinska.agnieszka.lucja@gmail.com>
2014-12-28 22:53:56 +0100

    add files

---
+++ script.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $('#includedLvl').load('lvl.html');
+});
---
+++ scriptLvl.js
@@ -0,0 +1,30 @@
+var level = 0;
+var exp = 0;
+
+$(document).ready(function(){
+
+	$image = $('#image');
+	$(image).append(level);
+
+});
+
+$(function() {
+    $( "#progressbar" ).progressbar({
+      value: 0
+    })
+    .data("value","0");
+    
+    $("#but").click(function() {
+        var currValue = $( "#progressbar" ).data("value");
+        currValue = parseInt(currValue) ? parseInt(currValue) : 0;
+        if(currValue < 100) {
+            $( "#progressbar" ).progressbar({
+              value: currValue+1
+            }).data("value",currValue+1);
+            $("#exper").html((currValue+1)+" exp");
+        }    
+    });
+
+
+});
+
