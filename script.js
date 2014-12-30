fba25af92d108fadcd678840960ceb9ff5fae807
aczerwinska <czerwinska.agnieszka.lucja@gmail.com>
2014-12-30 22:26:55 +0100

    Cleaning

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
-    $('#includedLvl').load('lvl.html');
+
+	$('#image').append('0');
+
+});
+
+$(function() {
+    $( "#progressbar" ).progressbar({
+      value: 0
+    })
+    .data("value","0");
+    
+    $("#but").click(function() {
+        var currValue = $( "#progressbar" ).data("value");
+        currValue = parseInt(currValue) ? parseInt(currValue) : 0;
+        if(currValue < 100) {
+            $( "#progressbar" ).progressbar({
+              value: currValue+1
+            }).data("value",currValue+1);
+            $("#exper").html((currValue+1)+" exp");
+        }    
+    });
+
+
 });
