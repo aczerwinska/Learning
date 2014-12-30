0ed0377535c36ab6f31de017825af8c84b9b2392
aczerwinska <czerwinska.agnieszka.lucja@gmail.com>
2014-12-30 23:29:41 +0100

    incr

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-var level = '0';
+var level = 0;
+var max = 10;
 
 $(document).ready(function(){
 
 	$('#image').append(level);
+    $('#maks').append(max);
 
 });
 
@@ -12,16 +14,22 @@ $(function() {
     })
     .data("value","0");
     
-    $("#but").click(function() {
+    
+$("#but").click(function() {
         var currValue = $( "#progressbar" ).data("value");
         currValue = parseInt(currValue) ? parseInt(currValue) : 0;
-        if(currValue < 100) {
+        if(currValue < max) {
             $( "#progressbar" ).progressbar({
               value: currValue+1
             }).data("value",currValue+1);
             $("#exper").html((currValue+1)+" exp");
-        }    
+        }else {
+            level += 1;
+            max = parseInt(max*1.2);
+            currValue = 0;
+            console.log('next lvl. Level: ' + level + ' Max: ' + max + ' CurrValue: ' + currValue);
+            $('#image').html(level);
+            $('#maks').html(max);
+        }
     });
-
-
 });
