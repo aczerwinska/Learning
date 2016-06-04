6596aa43b7ac040772a9e4002799a410cabfe5d0
aczerwinska <czerwinska.agnieszka.lucja@gmail.com>
2016-06-05 00:21:26 +0200

    Second

--- Python/DjangoProject/MySite/urls.py
+++ Python/DjangoProject/MySite/urls.py
@@ -1,13 +1,11 @@
 from django.conf.urls import url
+
 from . import views
 
+app_name = 'mysite'
 urlpatterns = [
-    # ex: /mysite/
     url(r'^$', views.index, name='index'),
-    # ex: /mysite/5/
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # ex: /mysite/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /mysite/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
